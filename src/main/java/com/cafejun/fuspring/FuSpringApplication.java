d235f34fd04b9f0ad4a36a67f90cfac577158c98
tb25271@gmail.com <tb25271@gmail.com>
2022-12-10 15:23:55 +0900

    [project setting]

---
+++ src/main/java/com/cafejun/fuspring/FuSpringApplication.java
@@ -0,0 +1,13 @@
+package com.cafejun.fuspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cafejun/fuspring/FuSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cafejun.fuspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
