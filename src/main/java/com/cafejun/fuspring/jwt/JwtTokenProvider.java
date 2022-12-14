be5d1cbae1a949b24aada8ad1bca3835b7aa638a
jsshin <tb25271@gmail.com>
2022-12-14 11:02:19 +0900

    [frontend] login세팅

--- src/main/java/com/cafejun/fuspring/jwt/JwtTokenProvider.java
+++ src/main/java/com/cafejun/fuspring/jwt/JwtTokenProvider.java
@@ -22,7 +22,6 @@ import java.util.stream.Collectors;
 @Slf4j
 @Component
 public class JwtTokenProvider {
-//    private String secretKey = "fu-spring-security";
     private final Key key;
 
     public JwtTokenProvider(@Value("${jwt.secret}") String secretKey) {
@@ -33,7 +32,6 @@ public class JwtTokenProvider {
     // 유저 정보를 가지고 AccessToken, RefreshToken 을 생성하는 메서드
     public TokenInfo generateToken(Authentication authentication) {
         // 권한 가져오기
-        System.out.println("JwtTokenProvider.generateToken"+authentication);
         String authorities = authentication.getAuthorities().stream()
                 .map(GrantedAuthority::getAuthority)
                 .collect(Collectors.joining(","));
