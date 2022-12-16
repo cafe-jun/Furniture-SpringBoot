c3e7bdb87a0382a063e4087d633647298250c0ff
jsshin <tb25271@gmail.com>
2022-12-16 23:25:53 +0900

    [auth] JWT Update

--- src/main/java/com/cafejun/fuspring/member/CustomMemberDetailService.java
+++ src/main/java/com/cafejun/fuspring/member/CustomMemberDetailService.java
@@ -11,8 +11,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
 import java.util.Collections;
 
 @Service
@@ -22,8 +22,12 @@ public class CustomMemberDetailService implements UserDetailsService {
     private final MemberRepository memberRepository;
     private final PasswordEncoder passwordEncoder;
 
+    @Transactional(readOnly = true) // Transactional 범위는 유지하되 조회 기능만 남겨두어 조회속도가 개선됨
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        //.map() -> memberRepository로 넘어온 member의 Stream을 map을 통해 createUserDetails 실행
+        //.orElseThrow() -> 값이 있으면 리턴 없으면 UsernameNotFoundException 발생
+        // UsernameNotFoundException 가 실행이 안되는것 같다
         return memberRepository.findByEmail(email)
                 .map(this::createUserDetails)
                 .orElseThrow(() -> new UsernameNotFoundException("해당하는 유저를 찾을 수 없습니다."));
@@ -33,17 +37,18 @@ public class CustomMemberDetailService implements UserDetailsService {
     // 해당하는 User 의 데이터가 존재한다면 UserDetails 객체로 만들어서 리턴
     private UserDetails createUserDetails(Member member) {
         System.out.println("CustomMemberDetailService.createUserDetails" + member);
-//        GrantedAuthority grantedAuthority = new SimpleGrantedAuthority(member.getRoles().toString());
 
-//        return new User(
-//            String.valueOf(member.getId()),
-//            member.getPassword(),
-//            Collections.singleton(grantedAuthority)
-//        );
         return User.builder()
                 .username(member.getEmail())
                 .password(passwordEncoder.encode(member.getPassword()))
                 .roles(member.getRoles().toArray(new String[0]))
                 .build();
     }
+    //        GrantedAuthority grantedAuthority = new SimpleGrantedAuthority(member.getRoles().toString());
+
+//        return new User(
+//            String.valueOf(member.getId()),
+//            member.getPassword(),
+//            Collections.singleton(grantedAuthority)
+//        );
 }
--- src/main/java/com/cafejun/fuspring/member/Member.java
+++ src/main/java/com/cafejun/fuspring/member/Member.java
@@ -11,11 +11,21 @@ import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * 왠만하면 Entity 의 PK는 Long Type에 Auto_increment를 추천
+ * 이렇게 하면 bigInt 타입이 됨
+ * 주민등록번호와 같이 비즈니스상 유니크키나 여러키를 조합한 복합키를 사용하면 난감한 상황발생
+ * 1, PK를 맺을때는 다른 테이블에서 복합키를 전부 갖고 있거나 중간 테이블을 하나 더 구축해야하는 상황이 발생
+ * 2. 인덱스에 좋은 여향을 끼치지 못합니다.
+ * 3. 유니크한 조건이 변경될경우 PK 전체를 수정해야하는 일이 발생
+ *
+ */
+
 @Builder
 @Data
 @Entity
 @Table(name = "T_MEMBER")
-@NoArgsConstructor
+@NoArgsConstructor // 기본 생성자 자동추가
 @AllArgsConstructor
 public class Member implements UserDetails {
     @Id
