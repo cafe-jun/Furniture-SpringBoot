fab31f7dd1f3773d51cd7e9e5ef804c959af3326
jsshin <tb25271@gmail.com>
2022-12-20 18:42:11 +0900

    [auth] update

--- src/main/java/com/cafejun/fuspring/service/member/memberService.java
+++ src/main/java/com/cafejun/fuspring/service/member/memberService.java
@@ -1,4 +1,11 @@
 package com.cafejun.fuspring.service.member;
 
-public class memberService {
+import com.cafejun.fuspring.domain.entity.member.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface memberService extends JpaRepository<Member,Long> {
+    Optional<Member> findByEmail(String email);
+    Boolean existsByEmail(String email);
 }
